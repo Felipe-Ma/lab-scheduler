a0cd134d69eeea109b7eece8276f28cdeb433f03
Felipe-Ma710 <82612231+Felipe-Ma710@users.noreply.github.com>
2023-11-03 11:33:32 -0700

    added gitignore file

--- main.py
+++ main.py
@@ -15,13 +15,3 @@ def retrieve_server_info(server):
 if __name__ == '__main__':
     server = Server()
     retrieve_server_info(server)
-    # Print
-    #print(server)
-    #print(get_product_name())
-    #print(get_product_version())
-
-    # Call functions and fill in the server object
-    #method_name()
-
-    # Print
-    #print(server)
