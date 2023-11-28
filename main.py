d711dc8cbacf16d88dfebe46950a623c5d890bb0
Felipe-Ma710 <82612231+Felipe-Ma710@users.noreply.github.com>
2023-11-27 20:03:55 -0800

    testing for config
    Commented out the server functions so i can continue working on config, will uncomment later

--- main.py
+++ main.py
@@ -11,7 +11,19 @@ def retrieve_server_info(server):
     server.add_product_version(get_product_version())
     server()
 
+def retrieve_config_values(config):
+    config()
+
+    config.set_current_directory(get_current_directory())
+    config.set_config_path(get_config_path(config.current_directory))
+    config.set_credential_path(get_credential_path(config.config_path))
+
+    config.set_server_name(get_server_name(config.config_path))
+    config()
 
 if __name__ == '__main__':
-    server = Server()
-    retrieve_server_info(server)
+    #server = Server()
+    config = Config()
+    print(config)
+    retrieve_config_values(config)
+    #retrieve_server_info(server)
